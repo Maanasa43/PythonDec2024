2bf553a68fa14401d74b1b3f6d39ce83d7054c24
Maanasa43 <manasachalla87@gmail.com>
2025-02-03 03:18:15 +0000

    class02- class01

---
+++ class01/Indendation_types.py
@@ -0,0 +1,12 @@
+# Importance of Indentation
+
+print ("Hello World 1")
+
+print ("Hello World 1")
+print ("Hello World 2")
+print ("Hello World 3")
+
+# print ("Hello World 2") # unexpected indent
+
+#indentation errors and best practices
+
---
+++ class01/c hello.py
@@ -0,0 +1,13 @@
+"""
+purpose: Basic PEP 8 guidelines
+
+this is a doc string
+
+shebang line
+
+
+"""
+# print as a statement in python 2
+print("Hello world!")
+print (True)
+print (true, 123)
